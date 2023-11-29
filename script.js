aeb8edcc53023ee090d1031a5027cae09e5ca7d1
Ishan Aryan <133885758+ishan-aryan-8882@users.noreply.github.com>
2023-11-30 00:40:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('button').addEventListener('click', function() {
+    document.querySelector(".bright").classList.toggle('reduceopacity');
+    document.querySelector(".bright").classList.toggle('borderright');
+});
