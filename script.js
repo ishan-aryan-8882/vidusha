3c6ff388fd8613622e154b16e423ddd769efa769
Ishan Aryan <133885758+ishan-aryan-8882@users.noreply.github.com>
2023-12-01 23:24:49 +0530

    added cover image support for book

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+let showFace = false;
 document.querySelector('button').addEventListener('click', function() {
-    document.querySelector(".bright").classList.toggle('reduceopacity');
-    document.querySelector(".bright").classList.toggle('borderright');
+    const brightElement = document.querySelector(".bright");
+    brightElement.classList.toggle('reduceopacity');
+    brightElement.classList.toggle('borderright');
+    if (!showFace) {
+        brightElement.style.background = 'rgb(183, 190, 143)';
+        brightElement.style.backgroundImage = 'none';
+    } else {
+        let imgUrl = 'me.jpg';
+        brightElement.style.backgroundImage = `url(${imgUrl})`;
+        brightElement.style.backgroundSize = 'cover';
+    }
+    showFace = !showFace;
 });
